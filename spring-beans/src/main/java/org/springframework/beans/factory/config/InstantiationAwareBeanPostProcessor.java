99b739f86daea0d570e6173c6d02f63d2b613190
haisen <lhsaq2009@163.com>
2023-01-23 16:28:17 +0800

    InstantiationAwareBeanPostProcessor 100%

--- spring-beans/src/main/java/org/springframework/beans/factory/config/InstantiationAwareBeanPostProcessor.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/config/InstantiationAwareBeanPostProcessor.java
@@ -34,24 +34,53 @@ import org.springframework.lang.Nullable;
 public interface InstantiationAwareBeanPostProcessor extends BeanPostProcessor {
 
 	/**
+	 * 总结：<br/><br/>
+	 *    1) 在对象实例化前，直接返回一个对象 ( 如：代理对象 ) 来代替「通过正常流程创建的对象」，返回 non-null 对象将导致正常的实例化短路<br/><br/>
+	 *    2) 如果自定义一个代理对象并返回，那么就会使用定义的代理对象，如果返回 null，Spring 就会走正常实例化流程创建对象<br/><br/>
+	 *
+	 * <hr><br/>
+	 *
 	 * Apply this BeanPostProcessor <i>before the target bean gets instantiated</i>.
 	 * The returned bean object may be a proxy to use instead of the target bean,
-	 * effectively suppressing default instantiation of the target bean.
+	 * effectively suppressing default instantiation of the target bean.<br/><br/>
+	 *
+	 * 在目标 Bean 实例化之前，应用此 BeanPostProcessor。
+	 * 本方法返回的 Bean 对象，可以是要使用的代理而不是目标 Bean，
+	 * 从而有效地抑制了目标 Bean 的默认实例化。<br/><br/>
+	 *
+	 * <hr>
+	 *
 	 * <p>If a non-null object is returned by this method, the bean creation process
 	 * will be short-circuited. The only further processing applied is the
 	 * {@link #postProcessAfterInitialization} callback from the configured
 	 * {@link BeanPostProcessor BeanPostProcessors}.
 	 * <p>This callback will be applied to bean definitions with their bean class,
 	 * as well as to factory-method definitions in which case the returned bean type
-	 * will be passed in here.
+	 * will be passed in here.<br/><br/>
+	 *
+	 * 如果此方法返回 non-null 对象，则 Bean 创建过程将短路。应用的唯一进一步处理是来自已
+	 * 配置的 BeanPostProcessor.postProcessAfterlnitialization 回调。<br/><br/>
+	 *
+	 * 此回调将应用于 Bean definition 及其 Bean Class，以及 factory-method definitions，在这种情况下，返回的
+	 * Bean 类型将在此处传入。
+	 *
+	 * <hr>
+	 *
 	 * <p>Post-processors may implement the extended
 	 * {@link SmartInstantiationAwareBeanPostProcessor} interface in order
 	 * to predict the type of the bean object that they are going to return here.
-	 * <p>The default implementation returns {@code null}.
+	 * <p>The default implementation returns {@code null}.<br/><br/>
+	 *
+	 * Post-processors 可以实现扩展的 SmartinstantiationAwareBeanPostProcessor 接口，以便预测它们将在此处返回的 Bean 对象的类型。
+	 * 默认实现返回 null。<br/><br/>
+	 *
+	 * <hr>
+	 *
 	 * @param beanClass the class of the bean to be instantiated
 	 * @param beanName the name of the bean
 	 * @return the bean object to expose instead of a default instance of the target bean,
 	 * or {@code null} to proceed with default instantiation
+	 *
 	 * @throws org.springframework.beans.BeansException in case of errors
 	 * @see #postProcessAfterInstantiation
 	 * @see org.springframework.beans.factory.support.AbstractBeanDefinition#getBeanClass()
@@ -63,7 +92,8 @@ public interface InstantiationAwareBeanPostProcessor extends BeanPostProcessor {
 	}
 
 	/**
-	 * 在对象实例化完毕执行 populateBean 之前，如果返回 false 则 Spring 不再对对应的 bean 实例进行自动依赖注入。
+	 * 在对象实例化完毕执行 {@link org.springframework.beans.factory.support.AbstractAutowireCapableBeanFactory#populateBean} 填充属性之前，
+	 * 如果返回 false 则 Spring 不再对对应的 bean 实例进行自动依赖注入
 	 *
 	 * Perform operations after the bean has been instantiated, via a constructor or factory method,
 	 * but before Spring property population (from explicit properties or autowiring) occurs.
@@ -109,6 +139,10 @@ public interface InstantiationAwareBeanPostProcessor extends BeanPostProcessor {
 	}
 
 	/**
+	 * 这里是在 Spring 处理完默认的成员属性，应用到指定的 bean 之前进行回调，可以用来检查和修改属性，
+	 * 最终返回的 PropertyValues 会应用到 bean 中 @Autowired、@Resource 等就是根据这个回调来实现最终注入依赖的属性的。<br/><br/>
+	 * <hr><br/>
+	 *
 	 * Post-process the given property values before the factory applies them
 	 * to the given bean. Allows for checking whether all dependencies have been
 	 * satisfied, for example based on a "Required" annotation on bean property setters.
