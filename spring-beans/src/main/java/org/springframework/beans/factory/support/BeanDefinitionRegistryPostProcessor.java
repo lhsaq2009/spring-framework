82a151cfc9ce6bc58db68d49906e946eaf60be18
haisen <lhsaq2009@163.com>
2023-01-23 12:35:59 +0800

    BeanFactoryPostProcessor 注释

--- spring-beans/src/main/java/org/springframework/beans/factory/support/BeanDefinitionRegistryPostProcessor.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/support/BeanDefinitionRegistryPostProcessor.java
@@ -24,19 +24,33 @@ import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
  * the registration of further bean definitions <i>before</i> regular
  * BeanFactoryPostProcessor detection kicks in. In particular,
  * BeanDefinitionRegistryPostProcessor may register further bean definitions
- * which in turn define BeanFactoryPostProcessor instances.
+ * which in turn define BeanFactoryPostProcessor instances.<br/><br/>
+ *
+ * 对标准 {@link BeanFactoryPostProcessor} SPI 的扩展，
+ * 允许在常规 BeanFactoryPostProcessor 检测开始之前，注册更多的 bean definition。
+ * 特别是，BeanDefinitionRegistryPostProcessor 可以注册更多的 bean definition，
+ * 这些 bean definition 又定义了 BeanFactoryPostProcessor 实例。<br/><br/>
  *
  * @author Juergen Hoeller
  * @since 3.0.1
  * @see org.springframework.context.annotation.ConfigurationClassPostProcessor
+ *
+ * 注册 Bean 到 Spring 容器
  */
 public interface BeanDefinitionRegistryPostProcessor extends BeanFactoryPostProcessor {
 
 	/**
+	 * 提供了丰富的方法，来操作 BeanDefinition，判断、注册、移除等方法：
+	 *
 	 * Modify the application context's internal bean definition registry after its
 	 * standard initialization. All regular bean definitions will have been loaded,
 	 * but no beans will have been instantiated yet. This allows for adding further
-	 * bean definitions before the next post-processing phase kicks in.
+	 * bean definitions before the next post-processing phase kicks in.<br/><br/>
+	 *
+	 * 在标准初始化后，修改 application context's 的内部 Bean 定义注册表。
+	 * 所有常规的 Bean definition 都将被加载，但尚未实例化任何 bean。
+	 * 这允许在 next post-processing 阶段开始之前，添加更多的 Bean 定义。<br/><br/>
+	 *
 	 * @param registry the bean definition registry used by the application context
 	 * @throws org.springframework.beans.BeansException in case of errors
 	 */
