2efee09bba004fc81a1f744a8ea75e4d79ec920f
haisen <lhsaq2009@163.com>
2023-01-31 18:17:38 +0800

    解析 <aop:aspectj-autoproxy ../> Done

--- spring-aop/src/main/java/org/springframework/aop/config/AspectJAutoProxyBeanDefinitionParser.java
+++ spring-aop/src/main/java/org/springframework/aop/config/AspectJAutoProxyBeanDefinitionParser.java
@@ -25,16 +25,17 @@ class AspectJAutoProxyBeanDefinitionParser implements BeanDefinitionParser {
 	@Override
 	@Nullable
 	public BeanDefinition parse(Element element, ParserContext parserContext) {		// <aop:aspectj-autoproxy>
-		// 1、注册 AnnotationAwareAspectJAutoProxyCreator
+		// 01、AopNamespaceUtils 注册 AnnotationAwareAspectJAutoProxyCreator
 		AopNamespaceUtils.registerAspectJAnnotationAutoProxyCreatorIfNecessary(parserContext, element);		// 02、Sz^Rbp$g2#vU
 		extendBeanDefinition(element, parserContext);
 		return null;
 	}
 
 	private void extendBeanDefinition(Element element, ParserContext parserContext) {
-		// 01、beanDef = {RootBeanDefinition@2217} "Root bean: class [org.springframework.aop.aspectj.annotation.AnnotationAwareAspectJAutoProxyCreator]"
+		// 01、beanDef = {RootBeanDefinition@2217} "Root bean: class [AnnotationAwareAspectJAutoProxyCreator]"
 		BeanDefinition beanDef = parserContext.getRegistry().getBeanDefinition(AopConfigUtils.AUTO_PROXY_CREATOR_BEAN_NAME);
-		// 02、处理子标签 <aop:include/>，指定 @Aspect 类，支持正则表达式，符合该表达式的切面类才会被应用
+		// 02、处理子标签 <aop:include/>，TODO：指定 @Aspect 类，支持正则表达式，符合该表达式的切面类才会被应用
+		// 解析 <aop:include /> 添加到 AnnotationAwareAspectJAutoProxyCreator.includePatterns
 		if (element.hasChildNodes()) {
 			addIncludePatterns(element, parserContext, beanDef);
 		}
