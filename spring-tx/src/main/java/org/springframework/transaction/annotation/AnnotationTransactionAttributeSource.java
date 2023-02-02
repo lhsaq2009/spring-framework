d97e7d6f01c84ac08b09440b32f2adc98b952a23
haisen <lhsaq2009@163.com>
2023-02-02 20:44:08 +0800

    None

--- spring-tx/src/main/java/org/springframework/transaction/annotation/AnnotationTransactionAttributeSource.java
+++ spring-tx/src/main/java/org/springframework/transaction/annotation/AnnotationTransactionAttributeSource.java
@@ -14,6 +14,7 @@ import org.springframework.transaction.interceptor.TransactionAttribute;
 import org.springframework.util.Assert;
 import org.springframework.util.ClassUtils;
 
+
 /**
  * Implementation of the
  * {@link org.springframework.transaction.interceptor.TransactionAttributeSource}
