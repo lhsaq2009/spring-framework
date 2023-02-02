3199c1d058aae0b047c32e7f6a86437f9f64f66a
haisen <lhsaq2009@163.com>
2023-02-02 16:42:33 +0800

    None

--- spring-aop/src/main/java/org/springframework/aop/framework/JdkDynamicAopProxy.java
+++ spring-aop/src/main/java/org/springframework/aop/framework/JdkDynamicAopProxy.java
@@ -183,7 +183,7 @@ final class JdkDynamicAopProxy implements AopProxy, InvocationHandler, Serializa
 			target = targetSource.getTarget();	//
 			Class<?> targetClass = (target != null ? target.getClass() : null);
 
-			// Get the interception chain for this method.
+			// 获取此方法的拦截链；Get the interception chain for this method.
 			List<Object> chain = this.advised.getInterceptorsAndDynamicInterceptionAdvice(method, targetClass);  // <==
 
 			// Check whether we have any advice. If we don't, we can fallback on direct
