ff91d7491fcc9c8e843ceb47449b852d54b7d4d8
haisen <lhsaq2009@163.com>
2023-02-02 22:18:01 +0800

    None

--- spring-tx/src/main/java/org/springframework/transaction/annotation/Transactional.java
+++ spring-tx/src/main/java/org/springframework/transaction/annotation/Transactional.java
@@ -12,11 +12,19 @@ import org.springframework.transaction.TransactionDefinition;
 
 /**
  * Describes a transaction attribute on an individual method or on a class.
+ * 描述 单个方法 或 类上的事务属性 <br/><br/>
+ *
+ * <hr>
  *
  * <p>At the class level, this annotation applies as a default to all methods of
  * the declaring class and its subclasses. Note that it does not apply to ancestor
  * classes up the class hierarchy; methods need to be locally redeclared in order
- * to participate in a subclass-level annotation.
+ * to participate in a subclass-level annotation.<br/><br/>
+ *
+ * TODO (重要)：在「类级别」，此注释作为默认值应用于声明类及其子类的所有方法。
+ * TODO (重要)：请注意，它不适用于类层次结构中的祖先类; 方法需要在本地重新声明才能参与子类级别的注释。<br/><br/>
+ *
+ * <hr>
  *
  * <p>This annotation type is generally directly comparable to Spring's
  * {@link org.springframework.transaction.interceptor.RuleBasedTransactionAttribute}
@@ -24,22 +32,43 @@ import org.springframework.transaction.TransactionDefinition;
  * convert the data to the latter class, so that Spring's transaction support code
  * does not have to know about annotations. If no custom rollback rules apply,
  * the transaction will roll back on {@link RuntimeException} and {@link Error}
- * but not on checked exceptions.
+ * but not on checked exceptions. <br/><br/>
+ *
+ * 这种注解类型一般可以直接与 Spring 的 org.springframework.transaction.interceptor.RuleBasedTransactionAttribute 类相媲美，
+ * 实际上 AnnotationTransactionAttributeSource 会将数据直接转换为后一类，这样 Spring 的事务支持代码就不必知道注解了。
+ * 如果未应用自定义回滚规则，则事务将在运行时异常和错误时回滚，但不在选中的异常上回滚。<br/><br/>
+ *
+ * <hr>
  *
  * <p>For specific information about the semantics of this annotation's attributes,
  * consult the {@link org.springframework.transaction.TransactionDefinition} and
- * {@link org.springframework.transaction.interceptor.TransactionAttribute} javadocs.
+ * {@link org.springframework.transaction.interceptor.TransactionAttribute} javadocs.<br/><br/>
+ *
+ * 有关此注解属性语义的特定信息，请参阅 TransactionDefinition 和
+ * org.springframework.transaction.interceptor.TransactionAttribute javadocs。<br/><br/>
+ *
+ * <hr>
  *
  * <p>This annotation commonly works with thread-bound transactions managed by
  * {@link org.springframework.transaction.PlatformTransactionManager}, exposing a
  * transaction to all data access operations within the current execution thread.
- * <b>Note: This does NOT propagate to newly started threads within the method.</b>
+ * <b>Note: This does NOT propagate to newly started threads within the method.</b><br/><br/>
+ *
+ * 此注释通常适用于由 org.springframework.transaction.PlatformTransactionManager
+ * 管理的线程绑定事务，将事务公开给当前执行线程中的所有数据访问操作。注意：这不会传播到方法中新启动的线程。<br/><br/>
+ *
+ * <hr>
  *
  * <p>Alternatively, this annotation may demarcate a reactive transaction managed
  * by {@link org.springframework.transaction.ReactiveTransactionManager} which
  * uses the Reactor context instead of thread-local attributes. As a consequence,
  * all participating data access operations need to execute within the same
- * Reactor context in the same reactive pipeline.
+ * Reactor context in the same reactive pipeline.<br/><br/>
+ *
+ * 或者，此注释可以划分由org.springframework.transaction.ReactiveTransactionManager管理的反应式事务，
+ * 该事务使用Actor上下文而不是线程本地属性。因此，所有参与的数据访问操作都需要在同一反应式管道的同一 Reactor 上下文中执行。<br/><br/>
+ *
+ * <hr>
  *
  * @author Colin Sampaleanu
  * @author Juergen Hoeller
