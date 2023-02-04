eadb2a1caf9ead443481bb6b6c1e4e6ea69a9fed
haisen <lhsaq2009@163.com>
2023-02-04 15:57:45 +0800

    None

--- spring-aop/src/main/java/org/springframework/aop/aspectj/AspectJProxyUtils.java
+++ spring-aop/src/main/java/org/springframework/aop/aspectj/AspectJProxyUtils.java
@@ -14,14 +14,23 @@ import org.springframework.aop.interceptor.ExposeInvocationInterceptor;
  * @author Juergen Hoeller
  * @since 2.0
  */
-public abstract class AspectJProxyUtils {
+public abstract class AspectJProxyUtils {		//
 
 	/**
 	 * Add special advisors if necessary to work with a proxy chain that contains AspectJ advisors:
-	 * concretely, {@link ExposeInvocationInterceptor} at the beginning of the list.
+	 * concretely, {@link ExposeInvocationInterceptor} at the beginning of the list.<br/><br/>
+	 *
 	 * <p>This will expose the current Spring AOP invocation (necessary for some AspectJ pointcut
 	 * matching) and make available the current AspectJ JoinPoint. The call will have no effect
-	 * if there are no AspectJ advisors in the advisor chain.
+	 * if there are no AspectJ advisors in the advisor chain.<br/><br/>
+	 *
+	 * 如有必要，添加特殊顾问，以使用包含 AspectJ advisors 的代理链：具体来说，在列表开头的 {@link ExposeInvocationInterceptor}。
+	 *
+	 * 这将公开当前的 Spring AOP 调用（对于某些 AspectJ pointcut 匹配 是必需的），并使当前的 AspectJ JoinPoint 可用。
+	 * 如果 advisor chain 中没有 AspectJ advisors，则调用将不起作用。<br/><br/>
+	 *
+	 * <hr>
+	 *
 	 * @param advisors the advisors available
 	 * @return {@code true} if an {@link ExposeInvocationInterceptor} was added to the list,
 	 * otherwise {@code false}
@@ -34,12 +43,12 @@ public abstract class AspectJProxyUtils {
 				// Be careful not to get the Advice without a guard, as this might eagerly
 				// instantiate a non-singleton AspectJ aspect...
 				if (isAspectJAdvice(advisor)) {
-					foundAspectJAdvice = true;
+					foundAspectJAdvice = true;		// <aop:advisor
 					break;
 				}
 			}
 			if (foundAspectJAdvice && !advisors.contains(ExposeInvocationInterceptor.ADVISOR)) {
-				advisors.add(0, ExposeInvocationInterceptor.ADVISOR);
+				advisors.add(0, ExposeInvocationInterceptor.ADVISOR);		// <== 添加到调用链头部
 				return true;
 			}
 		}
@@ -51,7 +60,10 @@ public abstract class AspectJProxyUtils {
 	 * @param advisor the Advisor to check
 	 */
 	private static boolean isAspectJAdvice(Advisor advisor) {
-		return (advisor instanceof InstantiationModelAwarePointcutAdvisor ||
+		return (
+				// @Aspect
+				advisor instanceof InstantiationModelAwarePointcutAdvisor ||
+
 				advisor.getAdvice() instanceof AbstractAspectJAdvice ||
 				(advisor instanceof PointcutAdvisor &&
 						((PointcutAdvisor) advisor).getPointcut() instanceof AspectJExpressionPointcut));
