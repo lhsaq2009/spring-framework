97a44efa6b8c2cc20a2d9d4f18ed9859541678cc
haisen <lhsaq2009@163.com>
2023-02-04 21:21:09 +0800

    None

--- spring-aop/src/main/java/org/springframework/aop/framework/autoproxy/InfrastructureAdvisorAutoProxyCreator.java
+++ spring-aop/src/main/java/org/springframework/aop/framework/autoproxy/InfrastructureAdvisorAutoProxyCreator.java
@@ -5,11 +5,43 @@ import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.lang.Nullable;
 
 /**
+ * 仅考虑基础结构顾问程序 Bean 的 Auto-proxy 创建者，忽略任何应用程序定义的顾问程序。
+ *
  * Auto-proxy creator that considers infrastructure Advisor beans only,
  * ignoring any application-defined Advisors.
  *
  * @author Juergen Hoeller
  * @since 2.0.7
+ *
+ * -----------------------------------------------------------------
+ *
+ * public abstract class AopConfigUtils {
+ *
+ *     static {
+ *         // Set up the escalation list ...    设置升级列表 ...
+ *         APC_PRIORITY_LIST.add(InfrastructureAdvisorAutoProxyCreator.class);		// 优先级最低
+ *         APC_PRIORITY_LIST.add(AspectJAwareAdvisorAutoProxyCreator.class);
+ *         APC_PRIORITY_LIST.add(AnnotationAwareAspectJAutoProxyCreator.class);		// 优先级最高
+ *     }
+ *
+ * -----------------------------------------------------------------
+ * TODO：2023-01-23，UML 类图，重新从 Monodraw 获取 ...
+ * -----------------------------------------------------------------
+ *
+ * 何时初始化：
+ * 		==> getBean("internalAutoProxyCreator")
+ * 			=>> getSingleton(..)
+ *				=>> initializeBean(..)
+ *					=>> invokeAwareMethods(beanName, bean);
+ *						   ((BeanNameAware) bean).setBeanName(beanName);
+ * 					((BeanClassLoaderAware) bean).setBeanClassLoader(bcl);
+ * 						((BeanFactoryAware) bean).setBeanFactory(AbstractAutowireCapableBeanFactory.this);
+ * 						=>> AbstractAdvisorAutoProxyCreator.setBeanFactory(..)
+ * 					       	{@link org.springframework.aop.framework.autoproxy.AbstractAdvisorAutoProxyCreator#setBeanFactory}
+ * 						    =>> {@link InfrastructureAdvisorAutoProxyCreator#initBeanFactory}
+ *
+ * <**:annotation-driven ...>
+ * @EnableTransactionManagement & @EnableCaching
  */
 @SuppressWarnings("serial")
 public class InfrastructureAdvisorAutoProxyCreator extends AbstractAdvisorAutoProxyCreator {
